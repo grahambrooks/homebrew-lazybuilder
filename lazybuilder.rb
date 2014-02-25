d488c840704af20447ea35f89e767519eed1e9e3
Graham Brooks <graham@grahambrooks.com>
2014-02-25 17:25:31 -0500

    updated version number

--- lazybuilder.rb
+++ lazybuilder.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Lazybuilder < Formula
   homepage 'http://github.com/grahambrooks/lazybuilder'
-  url 'https://github.com/grahambrooks/lazybuilder/archive/0.3.tar.gz'
+  url 'https://github.com/grahambrooks/lazybuilder/archive/0.4.tar.gz'
   sha1 'c5ca4a058892585526355b42d04fd40df0790ec9'
 
   head 'https://github.com/grahambrooks/lazybuilder.git'
