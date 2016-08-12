ecb446f1ec1d0429f0028c47822c0791fb36e370
Graham <graham@grahambrooks.com>
2016-08-12 07:34:57 -0600

    Update lazybuilder.rb
    Updated to use a sha256 signature for continued home-brew support

--- lazybuilder.rb
+++ lazybuilder.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Lazybuilder < Formula
   homepage 'http://github.com/grahambrooks/lazybuilder'
   url 'https://github.com/grahambrooks/lazybuilder/archive/0.7.tar.gz'
-  sha1 'b92eb404860c07dd71e0dc2113c1aed56d2cd729'
+  sha256 'de74481c8e18a77ea4689dd960fa08b680dc11b3551e3552e4e8b2cdadb17553'
 
   head 'https://github.com/grahambrooks/lazybuilder.git'
 
